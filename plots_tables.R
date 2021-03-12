e1d2485d2da9209bf0795eb8386ba0b3bb44ad4a
bartas0215 <poz.bartosznowak@gmail.com>
2021-03-12 22:09:59 +0100

    Function for data analysis

--- plots_tables.R
+++ plots_tables.R
@@ -3,14 +3,17 @@ install.packages("pastecs")
 
 #Load packages
 library(pastecs)
+library(tidyverse)
+library(xlsx)
 
 # Setting options
 options(scipen=999)
 
-### Save plot regarding drug
+# Load function for analysis
 
-drug_f <- function(x,y) {
+drug_f <- function(x,y,z) {
 
+### Save plot regarding drug  
 # Filter data 
 drug_graph <- df_3 %>%
   filter(Scope == {{x}}) %>%
@@ -19,30 +22,31 @@ drug_graph <- df_3 %>%
 # Save file 
 ggsave(plot = drug_graph,filename = y,path = "D:/covid_drugs/Images", device = "png", dpi = 600 )
 
-}
-
-drug_f(x = "acenocoumarol", y = "acenocoumarol.png")
-
-          ###FIX###
-
-drug_f_1 <- function(x,z) {
 
 ### Performing basic statistics
 # Pull Availability
 drug_1 <- df_3 %>%
-  filter(Scope == {{X}}) %>%
-  pull(Availability)
+  filter(Scope == {{x}}) %>%
+  stat.desc()
 
-drug_1 <- as_tibble(drug_1)
-
-#Perform basic statistics
-drug_2 <- stat.desc(drug_1)
+drug_2 <- drug_1 %>%
+  select(Availability)
 
 write.xlsx(drug_2,file = z)
 
 }
 
-drug_f_1(x = "acenocoumarol", z = "D:/covid_drugs/Tables/acenocoumarol")
+## Perform analysis 
 
+# Drugs with supply shortage
 
+drug_f(x = "acenocoumarol", y = "acenocoumarol.png", z = "D:/covid_drugs/Tables/acenocoumarol.xlsx")
+drug_f(x = "nitrendipine", y = "nitrendipine.png", z = "D:/covid_drugs/Tables/nitrendipine.xlsx")
+drug_f(x = "dalteparin", y = "dalteparin.png", z = "D:/covid_drugs/Tables/dalteparin.xlsx")
+drug_f(x = "diltiazem", y = "diltiazem.png", z = "D:/covid_drugs/Tables/diltiazem.xlsx")
 
+# Drugs which become unavailable during COVID pandemics
+
+drug_f(x = "prasugrel", y = "prasugrel.png", z = "D:/covid_drugs/Tables/prasugrel.xlsx")
+drug_f(x = "bemiparin", y = "bemiparin.png", z = "D:/covid_drugs/Tables/bemiparin.xlsx")
+drug_f(x = "exenatide", y = "exenatide.png", z = "D:/covid_drugs/Tables/exenatide.xlsx")
