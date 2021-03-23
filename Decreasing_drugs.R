1d66117871e8fffa931a08bee7fa9fc5d3b6dc5f
bartas0215 <poz.bartosznowak@gmail.com>
2021-03-24 00:07:58 +0100

    Script producing figures for manuscript

--- Decreasing_drugs.R
+++ Decreasing_drugs.R
@@ -15,24 +15,27 @@ df_4 <- df_3 %>%
 # Plot acenocoumarol
 ace <-df_3 %>%
   filter(Scope == "acenocoumarol") %>%
-  ggplot(aes(Date, Availability)) + geom_line() + scale_y_continuous(limits = c(0,100)) + theme(panel.background = element_rect(fill = 'grey93')) + 
-  ggtitle("Acenocumarol") 
+  ggplot(aes(Date, Availability)) + geom_line() + scale_y_continuous(limits = c(0,100)) + theme(panel.background = element_rect(fill = 'grey93'), axis.title.x=element_blank()) + 
+  ggtitle("Acenocumarol") + scale_x_date(labels = date_format("%m/%y")) + labs(y = "Availability [%]")
 
 #Plot nitrendipine
 nitr <- df_3 %>%
   filter(Scope == "nitrendipine") %>%
-  ggplot(aes(Date, Availability)) + geom_line() + scale_y_continuous(limits = c(0,100)) + theme(panel.background = element_rect(fill = 'grey93')) +
-  ggtitle("Nitrendipine")
+  ggplot(aes(Date, Availability)) + geom_line() + scale_y_continuous(limits = c(0,100)) + theme(panel.background = element_rect(fill = 'grey93'),axis.title.x=element_blank()) +
+  ggtitle("Nitrendipine") + scale_x_date(labels = date_format("%m/%y")) + labs(y = "Availability [%]")
 
 #Plot diltiazem
 dilt <-df_3 %>%
   filter(Scope == "diltiazem") %>%
-  ggplot(aes(Date, Availability)) + geom_line() + scale_y_continuous(limits = c(0,100)) + theme(panel.background = element_rect(fill = 'grey93')) +
-  ggtitle("Diltiazem")
+  ggplot(aes(Date, Availability)) + geom_line() + scale_y_continuous(limits = c(0,100)) + theme(panel.background = element_rect(fill = 'grey93'),axis.title.x=element_blank()) +
+  ggtitle("Diltiazem") + scale_x_date(labels = date_format("%m/%y")) + labs(y = "Availability [%]")
 
 
 # Align all plots
 dy <- ggarrange(ace, nitr, dilt, labels = c("A","B","C"), ncol = 1, nrow = 3)
+
+###FIX###
+annotate_figure(dy, left = text_grob("Availability [%]"), rot = 90)
 
 #Save on disc
 ggsave(plot = dy,filename = "decreasing_drugs.png",path = "D:/covid_drugs/Images", device = "png", dpi = 600 )
--- plots_2.0.R
+++ plots_2.0.R
@@ -3,26 +3,42 @@ library(tidyverse)
 library(xlsx)
 library(ggpubr)
 library(scales)
+library(scales)
+library(ggforce)
 
+# Median below 50 - manuscript ------------------------------------------
+
+
+# Find drugs with median availability below 50 % 
 df_4 <- df_3 %>% 
   group_by(Scope) %>%
   summarise(median_aval = median(Availability)) %>%
   filter(median_aval < 50)
 
+# Pull list of drugs from the median table
 df_5 <- df_4 %>%
   pull(Scope) %>%
   as.list()
 
+# Filter main df to have only the needed drugs
 df_6 <- df_3 %>% 
   filter(Scope %in% df_5) %>%
   as_tibble()
 
+# Plot all drugs in a list
 my_plots <- list( df_6 %>%
-  ggplot(aes(Date, Availability))  +  geom_line()+ facet_wrap(~ Scope)) + scale_x_date(labels = date_format("%m/%d")) +
-  theme(axis.text.x = element_text(angle=45))
+  ggplot(aes(Date, Availability))  +  geom_line()+ facet_wrap(~ Scope) + scale_x_date(labels = date_format("%m/%y"))+
+    theme(axis.title.x=element_blank(),axis.title.y = element_text(size = 25),panel.spacing.x = unit(1, "lines"))+ 
+    labs(y="Availability [%]"))
 
-xx <-ggarrange(plotlist = my_plots,nrow = ceiling(length(my_plots)),ncol = ceiling(length(my_plots)),
-          widths = 50, heights = 200)
+# Put all plots in one figure
+xx <-ggarrange(plotlist = my_plots)
 
-ggsave(plot = xx,filename = "c.png",path = "D:/covid_drugs/Images", device = "png", dpi = 600 )
+# Save results on disc
+ggsave(plot = xx,filename = "median_below_50.png",path = "D:/covid_drugs/Images", device = "png", dpi = 600,width = 17, height = 8, units = "in" )
+
+
+# Median above 50 % - supplementary  -------------------------------------------------------
+
+
           
