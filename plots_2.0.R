f88b3a8ef65c3645a1a8c17cfee3ad1905a9011f
bartas0215 <poz.bartosznowak@gmail.com>
2021-03-28 20:25:28 +0200

    Few additions

--- plots_2.0.R
+++ plots_2.0.R
@@ -4,6 +4,7 @@ library(xlsx)
 library(ggpubr)
 library(scales)
 library(ggforce)
+library(tools)
 
 # Median below 50 - manuscript ------------------------------------------
 
@@ -24,17 +25,37 @@ df_6 <- df_3 %>%
   filter(Scope %in% df_5) %>%
   as_tibble()
 
+# First letter to uppercase
+df_6 <- df_6 %>%
+ mutate(Scope = toTitleCase(Scope))
+
+df_6 <- df_6 %>%
+  mutate(Scope = str_replace_all(Scope, fixed("Medoxomil"), replacement = "medoxomil"))
+
+df_6$Scope<-gsub("+"," + ",df_6$Scope,fixed = TRUE)
+
+#Make space between plus sign
+df_6 <- apply(df_6,2,function(x)gsub('\\+', " + ",x))
+
+df_6 <- as_tibble(df_6)
+
+df_6$Date <- as.Date(df_6$Date, format= "%Y-%m-%d")
+
+df_6$Availability <- as.double(df_6$Availability)
+
+str(df_6)
+
 # Plot all drugs in a list
 my_plots <- list( df_6 %>%
-  ggplot(aes(Date, Availability))  +  geom_line()+ facet_wrap(~ Scope) + scale_x_date(labels = date_format("%m/%y"))+
-    theme(axis.title.x=element_blank(),axis.title.y = element_text(size = 25),panel.spacing.x = unit(1, "lines"))+ 
-    labs(y="Availability [%]"))
+  ggplot(aes(Date, Availability, group = Category))  +  geom_line()+ facet_wrap(~ Scope) + scale_x_date(labels = date_format("%m/%y"))+
+    theme(axis.title.x=element_blank(),axis.title.y = element_text(size = 40),panel.spacing.x = unit(1, "lines"))+ 
+    labs(y="Availability [%]") + theme_bw(base_size = 15))
 
 # Put all plots in one figure
 xx <-ggarrange(plotlist = my_plots)
 
 # Save results on disc
-ggsave(plot = xx,filename = "median_below_50.png",path = "D:/covid_drugs/Images", device = "png", dpi = 600,width = 17, height = 8, units = "in" )
+ggsave(plot = xx,filename = "median_below_50_1.png",path = "D:/covid_drugs/Images", device = "png", dpi = 600,width = 20, height = 8, units = "in" )
 
 
 # Median above 50 % - supplementary  -------------------------------------------------------
